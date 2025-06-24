7e30bf387a7df0458eb370b1449e03bf3a74af4c
Taeko-ar <lucasbaresm@gmail.com>
2025-06-24 15:28:35 -0300

    Feat: move e2e test to folder

---
+++ cypress/e2e/actions/actions.cy.ts
@@ -0,0 +1,14 @@
+import { actionsPage } from '../../pages/actionsPage';
+
+describe('Cypress actions commands page - ', () => {
+  it('should visit the given URL and check the title', () => {
+    actionsPage.visit();
+    cy.verify(cy.title(), 'include', 'Cypress.io');
+  });
+
+  it('should interact with an element', () => {
+    actionsPage.visit('/commands/actions');
+    actionsPage.typeUsername('fake@email.com');
+    cy.verify(actionsPage.elements.emailField(), 'have.value', 'fake@email.com')
+  });
+});
